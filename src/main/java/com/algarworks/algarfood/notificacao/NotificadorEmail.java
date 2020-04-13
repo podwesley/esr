34605f270477f8553c88424b73edb37dc1f60ebf
Wesley Rodrigues <podwesley@gmail.com>
2020-04-13 02:54:24 -0300

    2.26. Acessando propriedades com @ConfigurationProperties

--- src/main/java/com/algarworks/algarfood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algarworks/algarfood/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algarworks.algarfood.notificacao;
 
 import com.algarworks.algarfood.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -12,15 +13,13 @@ public class NotificadorEmail implements Notificador {
 
     private boolean caixaAlta;
 
-    @Value("${notificador.email.host-servidor}")
-    private String host;
-    @Value("${notificador.email.porta-servidor}")
-    private Integer porta;
+    @Autowired
+    private NotificadorProperties properties;
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
 
-        System.out.printf("Host: %s porta: %s",host , porta);
+        System.out.printf("Host: %s porta: %s",properties.getHostServidor() , properties.getPortaServidor());
 
         if (this.caixaAlta)
             mensagem = mensagem.toUpperCase();
