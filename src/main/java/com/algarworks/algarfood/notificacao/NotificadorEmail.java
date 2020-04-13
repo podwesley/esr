bca54f0a0a6636a2f521d79e498e0747c224322d
Wesley Rodrigues <podwesley@gmail.com>
2020-04-13 03:14:17 -0300

    2.27. Alterando a configuração do projeto dependendo do ambiente (com Spring Profiles)

--- src/main/java/com/algarworks/algarfood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algarworks/algarfood/notificacao/NotificadorEmail.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
 @TipoNotificador(NivelNotificacao.EMAIL)
 @Component
 public class NotificadorEmail implements Notificador {
@@ -16,10 +15,17 @@ public class NotificadorEmail implements Notificador {
     @Autowired
     private NotificadorProperties properties;
 
+    @Value("${notificador.email.host-servidor}")
+    private String servidor;
+
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
 
         System.out.printf("Host: %s porta: %s",properties.getHostServidor() , properties.getPortaServidor());
+        System.out.printf("\nHostTeste: %s portaTeste: %s\n",servidor , porta);
 
         if (this.caixaAlta)
             mensagem = mensagem.toUpperCase();
