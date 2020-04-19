1720a6940b1c44852685921d878a7fb21164efc6
Wesley Rodrigues <podwesley@gmail.com>
2020-04-18 22:12:16 -0300

    atualização para as aulas de spring data JPA.

--- src/main/java/com/algarworks/algarfood/notificacao/NivelNotificacao.java
+++
@@ -1,6 +0,0 @@
-package com.algarworks.algarfood.notificacao;
-
-public enum NivelNotificacao {
-
-    EMAIL, SMS
-}
--- src/main/java/com/algarworks/algarfood/notificacao/Notificador.java
+++
@@ -1,8 +0,0 @@
-package com.algarworks.algarfood.notificacao;
-
-import com.algarworks.algarfood.modelo.Cliente;
-
-public interface Notificador {
-
-    void notificar(Cliente cliente, String mensagem);
-}
