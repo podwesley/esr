33c062af63d4e45173b2418caa4ec0129c0b78be
Wesley Rodrigues <podwesley@gmail.com>
2020-05-16 18:55:10 -0300

    ajuste

--- src/main/java/com/algaworks/algafood/domain/entity/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/entity/Cozinha.java
@@ -12,34 +12,5 @@ public class Cozinha {
 
     @Column(name = "nome_cozinha")
     private String nome;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Cozinha)) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return getId().equals(cozinha.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
+    
 }
